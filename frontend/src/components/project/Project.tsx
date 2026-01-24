84b4b06a4f4e5b1153c158026203a48663e64302
Devsim <fdudevsim@gmail.com>
2026-01-24 15:51:11 +1100

    Update Project.tsx

--- frontend/src/components/project/Project.tsx
+++ frontend/src/components/project/Project.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import Image from "next/image";
 import { 
   ExternalLink, 
